82e8e663977308c3ac13529fff610614ddca9866
mchzh <mchzh_qlj@hotmail.com>
2020-08-22 11:37:08 -0700

    Create 497. Random Point in Non-overlapping Rectangles.java

---
+++ Math/497. Random Point in Non-overlapping Rectangles/497. Random Point in Non-overlapping Rectangles.java
@@ -0,0 +1,38 @@
+class Solution {
+    // convert to 1D area array
+    int[] areas;
+    int sum;
+    int[][] rects;
+    public Solution(int[][] rects) {
+        int N = rects.length;
+        sum = 0;
+        areas = new int[N];
+        this.rects = rects;
+        for (int i = 0; i < N; i++) {
+            sum += (rects[i][3]- rects[i][1]+1) * (rects[i][2]- rects[i][0]+1); // wid * hei
+            areas[i] = sum;
+        }
+        
+    }
+    
+    public int[] pick() {
+        Random rand = new Random();
+        int num = rand.nextInt(sum) + 1;
+        //System.out.println(num);
+        int ithRect = Arrays.binarySearch(areas, num);
+        if (ithRect < 0) {
+            ithRect = (-1) * ithRect -1;
+        }
+        int height = rects[ithRect][3]-rects[ithRect][1]+1;
+        int width = rects[ithRect][2]-rects[ithRect][0]+1;
+        Random randHei = new Random();
+        Random randWid = new Random();
+        return new int[] {rects[ithRect][0]+randWid.nextInt(width), rects[ithRect][1]+randHei.nextInt(height)};
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(rects);
+ * int[] param_1 = obj.pick();
+ */
