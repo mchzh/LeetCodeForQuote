d06daacd23ffed1cfe2aa824a29e0d3b04ea3a68
mchzh <mchzh_qlj@hotmail.com>
2020-09-02 13:52:27 -0700

    Create 220. Contains Duplicate III-performance.java

---
+++ TreeMap/220. Contains Duplicate III/220. Contains Duplicate III-performance.java
@@ -0,0 +1,24 @@
+class Solution {
+	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+		// corner case
+		if (nums.length < 2 || k == 0) {
+			return false;
+		}
+
+		// normal case
+		TreeSet<Long> set = new TreeSet<>();
+		int i = 0;
+		while (i < nums.length) {
+			Long floor = set.floor((long) nums[i]);
+			Long ceiling = set.ceiling((long) nums[i]);
+			if ((floor != null && nums[i] - floor <= t) || (ceiling != null && ceiling - nums[i] <= t)) {
+				return true;
+			}
+			set.add((long) nums[i++]);
+			if (i > k) {
+				set.remove((long) nums[i - k - 1]);
+			}
+		}
+		return false;
+	}
+}
