79c3bc77f392ca2fb5ef3f3a7bbb027db834e7c6
mchzh <mchzh_qlj@hotmail.com>
2020-09-13 12:20:48 -0700

    Create 986. Interval List Intersections.java

---
+++ Sort/986. Interval List Intersections/986. Interval List Intersections.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        // double pointer
+        int lena = A.length, lenb = B.length;
+        int first = 0, second = 0;
+        List<int[]> middle = new ArrayList<>();
+        while (first < lena || second < lenb) {
+            if (first < lena && second < lenb) {
+                
+                // no intersection
+                if (A[first][1] < B[second][0]) {
+                    //middle.add(A[first]);
+                    first++;
+                } else if (B[second][1] < A[first][0]) {
+                    //middle.add(B[second]);
+                    second++;
+                } else { // intersection
+                    int[] temp = new int[2];
+                    temp[0] = Math.max(A[first][0], B[second][0]);
+                    temp[1] = Math.min(A[first][1], B[second][1]);
+                    middle.add(temp);
+                    if (A[first][1] <= B[second][1]) {
+                        first++;
+                    } else {
+                        second++;
+                    }
+                }
+            } else if (first < lena) {
+                //middle.add(A[first]);
+                first++;
+            } else {
+                //middle.add(B[second]);
+                second++;
+            }
+        }
+                                            
+        // convert list to int array with stream
+        return middle.toArray(new int[middle.size()][2]);
+    }
+}
