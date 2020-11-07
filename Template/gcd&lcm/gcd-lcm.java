04858a7165eea88e2ebe8b99182f19ff9508f70c
mchzh <mchzh_qlj@hotmail.com>
2020-11-07 11:47:25 -0800

    Create gcd-lcm.java

---
+++ Template/gcd&lcm/gcd-lcm.java
@@ -0,0 +1,17 @@
+class Solution {
+    // stack
+    // https://leetcode.com/problems/fraction-addition-and-subtraction/discuss/103408/Java-Solution-Fraction-Addition-and-GCD
+    // gcd or lcm
+    public String fractionAddition(String expression) {
+        
+    }
+    //gcd
+    private int getGCD(int a, int b) {
+        if (a == 0 || b == 0) return a+b;
+        return getGCD(b, a%b);
+    }
+    private int getLCM(int a, int b) {
+        int gcd = getGCD(a, b);
+        return a/gcd * b /gcd;
+    }
+}
