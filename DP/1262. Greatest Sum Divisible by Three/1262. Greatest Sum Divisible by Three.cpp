98b18f5b726a0b227c9f3ea3e0fabc79515a5f5e
mchzh <mchzh_qlj@hotmail.com>
2020-11-26 15:42:23 -0500

    Create 1262. Greatest Sum Divisible by Three.cpp

---
+++ DP/1262. Greatest Sum Divisible by Three/1262. Greatest Sum Divisible by Three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        vector<int> dp(3);    
+        for (int num : nums) {
+          vector<int> tmp(dp);
+          for (int s : tmp)
+            dp[(s + num) % 3] = max(dp[(s + num) % 3], s + num);      
+        }
+        return dp[0];
+    }
+};
