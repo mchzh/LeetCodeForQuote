5792415b8ddf0710fe4ddcf3a6fdd5eb629cd1c6
mchzh <mchzh_qlj@hotmail.com>
2021-01-25 15:51:15 -0500

    Create 1151. Minimum Swaps to Group All 1's Together.java

---
+++ Greedy/1151. Minimum Swaps to Group All 1's Together/1151. Minimum Swaps to Group All 1's Together.java
@@ -0,0 +1,19 @@
+class Solution {
+    // slide window for the fixed size
+    public int minSwaps(int[] data) {
+        int n = data.length;
+        int[] countzero = new int[n+1];
+        int count = 0, countone = 0;
+        for (int i = 0; i < n; i++) {
+            if (data[i] == 0) count++;
+            countzero[i+1] = count;
+        }
+        int size = n-count;
+        //System.out.println(size + " " + count);
+        int rets = Integer.MAX_VALUE;
+        for (int i = size-1; i < n; i++) {
+            rets = Math.min(rets, countzero[i+1] - countzero[i+1-size]);
+        }
+        return rets;
+    }
+}
