e0ac2abaf54796d579fbf981383d8525d7000eb9
mchzh <mchzh_qlj@hotmail.com>
2021-02-24 23:16:08 -0500

    Update convertListToArray.java

--- Template/ListToArray/convertListToArray.java
+++ Template/ListToArray/convertListToArray.java
@@ -2,4 +2,7 @@ class Solution {
     public int[] covertIntegerArray(List<Integer> list) {
         return list.stream().mapToInt(i->i).toArray();
     }
+    public String[] covertStirngArray(List<String> list) {
+        return list.stream().toArray(n -> new String[n]); // list.stream ().toArray (String[]::new);
+    }
 }
