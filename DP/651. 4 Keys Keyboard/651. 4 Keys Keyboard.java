382b4dd988b8cea44a6da7dde1fe64e63c9e6daf
mchzh <mchzh_qlj@hotmail.com>
2021-03-02 00:59:38 -0500

    Create 651. 4 Keys Keyboard.java

---
+++ DP/651. 4 Keys Keyboard/651. 4 Keys Keyboard.java
@@ -0,0 +1,13 @@
+class Solution {
+    // CTRL + A C V is a compostion for i > 3
+    public int maxA(int N) {
+        int[] dp = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            dp[i] = i;
+            for (int j = 1; j < i-2; j++) {
+                dp[i] = Math.max(dp[i], dp[j]*(i-j-1));
+            }
+        }
+        return dp[N];
+    }
+}
