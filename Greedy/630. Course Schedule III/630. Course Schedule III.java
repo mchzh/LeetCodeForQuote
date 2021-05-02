924cefbc53d6b1b25f400ea83edf50bc2ce0c855
mchzh <mchzh_qlj@hotmail.com>
2021-05-02 11:57:42 -0700

    Create 630. Course Schedule III.java

---
+++ Greedy/630. Course Schedule III/630. Course Schedule III.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> (a[1] - b[1]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+            
+        int day = 0;
+        for (int i = 0; i < courses.length; i++) {
+            if (day + courses[i][0] <= courses[i][1]) {
+                day = day + courses[i][0];
+                pq.offer(courses[i][0]);
+            } else {
+                // overlap
+                pq.offer(courses[i][0]);
+                day += courses[i][0];  
+                day -= pq.poll();
+            }
+        }
+        return pq.size();
+    }
+}
