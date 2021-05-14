b1f2eadc5836fdf2c59d98153bb8bcc630c7c32b
mchzh <mchzh_qlj@hotmail.com>
2021-05-13 19:42:07 -0700

    Create 494. Target Sum-dp.cpp

---
+++ DP/494. Target Sum/494. Target Sum-dp.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<int, int>Map;
+        
+        Map[0]=1;
+        for (auto x : nums) {
+            auto Map_temp = Map;
+            Map.clear();
+            for (auto a : Map_temp) {
+                Map[a.first+x] += a.second;
+                Map[a.first-x] += a.second;
+            }
+        }
+        
+        return Map[target];
+    }
+};
